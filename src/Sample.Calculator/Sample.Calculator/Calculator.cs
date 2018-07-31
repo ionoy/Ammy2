6a4654470b8d8a6d9a82f59f5f6dc453b64a8dc4
ionoy <mihhail.maslakov@gmail.com>
2018-07-31 10:42:38 +0300

    Calculator refactor

--- src/Sample.Calculator/Sample.Calculator/Calculator.cs
+++ src/Sample.Calculator/Sample.Calculator/Calculator.cs
@@ -41,30 +41,35 @@ namespace Sample.Calculator
             }
 
             return Grid.Rows(6).Cols(4)
-                       .Padding(new Thickness(10))
+                       .Padding(new Thickness(20))                       
                        .Children(
                            Label.Grid_ColumnSpan(4)
-                               .FontSize(32).FontAttributes(FontAttributes.Bold)
+                               .FontSize(60).FontAttributes(FontAttributes.Bold)
                                .HorizontalTextAlignment(TextAlignment.End)
                                .VerticalTextAlignment(TextAlignment.Center)
                                .Text(currentValue, v => v.ToString()),
                            BuildNumberPad(appendNumber),
-                           Button.Text("div/").Grid_RowCol(1, 3).Command(() => applyOperator((accu, val) => accu / val)),
-                           Button.Text("*").Grid_RowCol(2, 3).Command(() => applyOperator((accu, val) => accu * val)),
-                           Button.Text("-").Grid_RowCol(3, 3).Command(() => applyOperator((accu, val) => accu - val)),
-                           Button.Text("+").Grid_RowCol(4, 3).Command(() => applyOperator((accu, val) => accu + val)),
-                           Button.Text("C").Grid_RowCol(5, 0).Command(clear),
-                           Button.Text("=").Grid_RowCol(5, 1).Command(() => applyOperator((accu, _) => accu)).Grid_ColumnSpan(3)
+                           NumberPadButton("/").Grid_RowCol(1, 3).Command(() => applyOperator((accu, val) => accu / val)),
+                           NumberPadButton("*").Grid_RowCol(2, 3).Command(() => applyOperator((accu, val) => accu * val)),
+                           NumberPadButton("-").Grid_RowCol(3, 3).Command(() => applyOperator((accu, val) => accu - val)),
+                           NumberPadButton("+").Grid_RowCol(4, 3).Command(() => applyOperator((accu, val) => accu + val)),
+                           NumberPadButton("C").Grid_RowCol(5, 0).Command(clear),
+                           NumberPadButton("=").Grid_RowCol(5, 1).Command(() => applyOperator((accu, _) => accu)).Grid_ColumnSpan(3)
                        );
         }
 
-        IEnumerable<Button> BuildNumberPad(Action<int> appendNumber) =>
-            Enumerable.Range(0, 10).Select(i => {
-                return Button.Grid_RowCol(i == 0 ? 4 : 4 - ((i + 2) / 3),
-                                          i == 0 ? 0 : (i + 2) % 3)
-                             .Grid_ColumnSpan(i == 0 ? 3 : 1)
-                             .Command(() => appendNumber(i))
-                             .Text(i.ToString());
+        IEnumerable<Button> BuildNumberPad(Action<int> appendNumber)
+        {
+            return Enumerable.Range(0, 10).Select(i =>
+            {
+                return NumberPadButton(i.ToString())
+                            .Grid_RowCol(i == 0 ? 4 : 4 - ((i + 2) / 3),
+                                         i == 0 ? 0 : (i + 2) % 3)
+                            .Grid_ColumnSpan(i == 0 ? 3 : 1)                                      
+                            .Command(() => appendNumber(i));
             });
+        }
+
+        Button NumberPadButton(string text) => Button.FontSize(32).Text(text);
     }
 }
